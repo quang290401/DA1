d7c4e4110ea0c4b2dcdb3afc24b4dc39be60f6fc
sonngph2212 <sonngph31538@gmail.com>
2024-03-16 02:15:20 +0700

    chore: update code

--- src/main/java/com/mycompany/da1/util/HibernateUltil.java
+++ src/main/java/com/mycompany/da1/util/HibernateUltil.java
@@ -22,9 +22,9 @@ public class HibernateUltil {
         properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
         properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=DUAN1");
         properties.put(Environment.USER, "sa");
-        properties.put(Environment.PASS, "quang@201");
+        properties.put(Environment.PASS, "22122002starx");
         properties.put(Environment.SHOW_SQL, "true");
-        properties.put(Environment.HBM2DDL_AUTO, "update"); // Hoặc "update" tùy vào nhu cầu của bạn
+        properties.put(Environment.HBM2DDL_AUTO, "create"); // Hoặc "update" tùy vào nhu cầu của bạn
 
         conf.setProperties(properties);
         conf.addAnnotatedClass(SanPhamEntity.class);
